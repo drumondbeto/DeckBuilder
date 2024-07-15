f0dbe976dcfc3e13b1808080dde865083963e9ba
Humberto Drumond <humbertowdrumond@gmail.com>
2024-07-14 23:42:23 -0300

    Creating MyCards and Simuator components and Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MyCardsComponent } from './components/my-cards/my-cards.component';
+import { SimulatorComponent } from './components/simulator/simulator.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'my-cards', component: MyCardsComponent },
+  { path: 'simulator', component: SimulatorComponent },
+];
